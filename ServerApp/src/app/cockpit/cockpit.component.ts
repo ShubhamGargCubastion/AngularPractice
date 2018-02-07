adb8c8f6a91c5c32a06ed5800752543047c801bf
ShubhamGargCubastion <shubham.garg@cubastion.com>
2018-02-07 15:50:16 +0530

    Communication Between Components using @Input() & @Output()

---
+++ ServerApp/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,37 @@
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{serverName:string, serverContent: string}>();
+  @Output() blueprintCreated = new EventEmitter<{serverName:string, serverContent: string}>();
+    // newServerName= '';
+    //newServerContent = '';
+    @ViewChild('serverContentInput') serverContentInput: ElementRef;    //local reference inside ViewChild()
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddServer(NameInput: HTMLInputElement) {
+    this.serverCreated.emit({
+      //    serverName:this.newServerName,
+      serverName:NameInput.value,
+//        serverContent:this.newServerContent
+      serverContent:this.serverContentInput.nativeElement.value
+    });
+  }
+
+  onAddBlueprint(NameInput: HTMLInputElement) {
+      this.blueprintCreated.emit({
+  //    serverName:this.newServerName,
+        serverName:NameInput.value,
+//      serverContent:this.newServerContent
+        serverContent:this.serverContentInput.nativeElement.value
+      });
+  }
+
+}
