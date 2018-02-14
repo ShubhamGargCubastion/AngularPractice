7f493eb2889695d3e22c3895091e562605e2cec8
ShubhamGargCubastion <shubham.garg@cubastion.com>
2018-02-14 15:54:33 +0530

    CourseApp using routes

---
+++ CourseAppService-Routes/src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient {
+  /*public ingredientName: string;
+  public amount: number;
+
+  constructor(name: string, amount: number) {
+    this.ingredientName = name;
+    this.amount = amount;
+  }*/
+      // OR
+  constructor(public ingredientName: string, public amount: number) { }
+
+}
