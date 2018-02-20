a0d0c2d570bfebef779727a9e641ee778d699d0e
ShubhamGargCubastion <shubham.garg@cubastion.com>
2018-02-20 12:34:20 +0530

    Http New Done

--- Http/src/app/app.component.ts
+++ Http/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {ServerService} from "./server.service";
+import { Response } from '@angular/http';
+
 
 @Component({
   selector: 'app-root',
@@ -10,21 +13,62 @@ export class AppComponent {
     {
       name: 'Testserver',
       capacity: 10,
-      id: this.generateId()
+      server_id: this.generateId()
     },
     {
       name: 'Liveserver',
       capacity: 100,
-      id: this.generateId()
+      server_id: this.generateId()
     }
   ];
-  onAddServer(name: string) {
-    this.servers.push({
-      name: name,
-      capacity: 50,
-      id: this.generateId()
-    });
+
+  createdServer: {name: string, capacity: number, server_id: number};
+  updatedServer: {id:number,name: string, capacity: number};
+
+
+  constructor(private serverService: ServerService){}
+
+  onSave(name:string,capacity:number) {
+
+    this.createdServer = {name: name, capacity: +capacity, server_id: this.generateId()};
+    this.serverService.storeServers(this.createdServer)
+      .subscribe(
+        (response) => {
+          console.log(response);
+          },
+      (error) => console.log(error)
+      );
   }
+  onGetServer() {
+    this.serverService.getServers()
+      .subscribe(
+        (servers:any[]) => {
+          console.log(servers);
+        },
+        (error) => console.log(error)
+      );
+  }
+
+  onUpdate(id:number,name:string,capacity:number) {
+    this.updatedServer = {id:+id,name: name, capacity: +capacity};
+    this.serverService.updateServer(this.updatedServer)
+      .subscribe(
+        (response) => {
+          console.log(response);
+        },
+        (error) => console.log(error)
+      );
+  }
+  onDelete(id:number) {
+    this.serverService.deleteServer(+id)
+      .subscribe(
+        (response) => {
+          console.log('Server Deleted');
+        },
+        (error) => console.log(error)
+      );
+  }
+
   private generateId() {
     return Math.round(Math.random() * 10000);
   }
---
+++ Http/src/app/server.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import 'rxjs/Rx';
+import {Response} from '@angular/http';
+
+@Injectable()
+
+export class ServerService {
+
+    constructor(private http: Http) { }
+    storeServers(servers: any) {
+      return this.http.post("http://localhost:8080/admin",servers);
+    }
+
+    getServers() {
+      return this.http.get("http://localhost:8080/admin")
+        .map(
+          (response:Response) => {
+            const data = response.json()
+            return data;
+          }
+        );
+    }
+
+    updateServer(server:any) {
+      return this.http.put("http://localhost:8080/admin",server);
+    }
+
+    deleteServer(id:number) {
+    return this.http.delete("http://localhost:8080/admin/"+id);
+  }
+}
